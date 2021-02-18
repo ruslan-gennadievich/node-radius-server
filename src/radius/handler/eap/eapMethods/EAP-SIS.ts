ead1eab74156e995a56e1ac7d8a36eee6fd0ca25
Ruslan Bychikhin <vb.xakep@gmail.com>
2021-02-18 17:30:21 +0200

    Норм получает сертификаты, сигнатуры итд

--- src/radius/handler/eap/eapMethods/EAP-SIS.ts
+++ src/radius/handler/eap/eapMethods/EAP-SIS.ts
@@ -36,37 +36,50 @@ export class EAPSIS implements IEAPMethod {
 				code: PacketResponseCode.AccessAccept,
 			};
 		}
+		const beginOffset = 2; // EAP Header size (value_size)
+		let [offset, len] = [beginOffset, 16];
+		const server_challenge = _msg.slice(offset, offset + len + 1);
 
-		const server_challenge = _msg.slice(1, 17);
-		const peer_challenge = _msg.slice(17, 33);
+		[offset, len] = [offset + len, 16];
+		const peer_challenge = _msg.slice(offset, offset + len + 1);
 
-		// Encryptor Sign
-		const ecr_sign_offset = _msg.slice(33, 35).readUInt16BE(0);
-		const ecr_sign_size = _msg.slice(35, 37).readUInt16BE(0);
-		// User Sign
-		const user_sign_offset = _msg.slice(37, 39).readUInt16BE(0);
-		const user_sign_size = _msg.slice(39, 41).readUInt16BE(0);
+		// Sign Encryptor
+		[offset, len] = [offset + len, 2];
+		const ecr_sign_offset = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
 
-		// Encryptor Cert
-		const ecr_cert_offset = _msg.slice(41, 43).readUInt16BE(0);
-		const ecr_cert_size = _msg.slice(43, 45).readUInt16BE(0);
-		// User Cert
-		const user_cert_offset = _msg.slice(45, 47).readUInt16BE(0);
-		const user_cert_size = _msg.slice(47, 49).readUInt16BE(0);
+		[offset, len] = [offset + len, 2];
+		const ecr_sign_size = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
+
+		// Sign User
+		[offset, len] = [offset + len, 2];
+		const user_sign_offset = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
+
+		[offset, len] = [offset + len, 2];
+		const user_sign_size = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
+
+		// Cert Encryptor
+		[offset, len] = [offset + len, 2];
+		const ecr_cert_offset = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
+
+		[offset, len] = [offset + len, 2];
+		const ecr_cert_size = _msg.slice(offset, offset + len).readUInt16LE(0);
+
+		// Cert User
+		[offset, len] = [offset + len, 2];
+		const user_cert_offset = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
+
+		[offset, len] = [offset + len, 2];
+		const user_cert_size = _msg.slice(offset, offset + len + 1).readUInt16LE(0);
+
 		// ======================
-		const encryptorSign = _msg.slice(50 + ecr_sign_offset, 49 + ecr_sign_size);
-		const userSign = _msg.slice(50 + user_sign_offset, 49 + user_sign_offset + user_sign_size);
+		const encryptorSign = _msg.slice(offset + len + ecr_sign_offset, offset + len + ecr_sign_size);
+		const userSign = _msg.slice(offset + len + user_sign_offset, offset + len + user_sign_offset + user_sign_size);
 
-		let encryptorCert = _msg.slice(50 + ecr_cert_offset, 49 + ecr_cert_offset + ecr_cert_size);
-		let userCert = _msg.slice(50 + user_cert_offset, 49 + user_cert_offset + user_cert_size);
+		const encryptorCert = _msg.slice(offset + len + ecr_cert_offset, offset + len + ecr_cert_offset + ecr_cert_size);
+		const userCert = _msg.slice(offset + len + user_cert_offset, offset + len + user_cert_offset + user_cert_size);
 
 		// ====== Parse encryptor cert =============
 		let certBase64 = '';
-
-		if (true) { //TEST
-			encryptorCert = fs.readFileSync('./4c0f5dddcdedfe88950dc36f697a50b11225c9e1.cer');
-			userCert = fs.readFileSync('./4c0f5dddcdedfe88950dc36f697a50b11225c9e1.cer');
-		}
 
 		if (encryptorCert[0] === 48) {
 			// if cert in DER
